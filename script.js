dc6edeca28dbf0f2f75df951ff0754207fc2cadd
CaptureB <airdavid62@gmail.com>
2023-02-28 17:12:51 -0700

    Fixed typos, added js file, fetch api works

--- script.js
+++ script.js
@@ -1,5 +1,47 @@
-const APIkey = "dc343b9765250b6d21dfd17c4cba48a5"
-const URL5DAY ="https://api.openweathermap.org/data/3.0/onecall?"
 
-let pastCities = []
+let searchIN = document.getElementById("lookUpCity")
+let searchBTN = document.getElementById("btnS")
+
+let listCities = []
+
+
+
+searchBTN.addEventListener("click",search)
+function search() {
+    let city = searchIN.value
+    listCities.push(city)
+    localStorage.setItem('Previous Searches',JSON.stringify(listCities))
+    getApi(city)
+    console.log(city)
+
+}
+
+function getApi(city) {
+    let urlRequest ="https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=dc343b9765250b6d21dfd17c4cba48a5&units=imperial";
+    fetch(urlRequest)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+        $("#yourCity").html(city)
+        $("#presentDay").text(dayjs().format('(MMM/D/YYYY)'))
+        $("#tempNow").html(data.main.temp + "℉")
+        $("#windNow").html(data.wind.speed + " MPH") 
+        $("#humidNow").html(data.main.humidity + "%")
+    })
+}
+
+function renderPreviousCities() {
+    let previousCities = JSON.parse(localStorage.getItem('Previous Searches'))
+    if (previousCities !== null) {
+        document.getElementById("pastCities").innerHTML = previousCities
+    } else {
+        return
+    }
+}
+
+searchBTN.addEventListener("click", renderPreviousCities)
+
+
+
 
