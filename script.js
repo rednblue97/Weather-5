38da4ebbad1712c9e57e83094ab4c726ac3d0c2b
CaptureB <airdavid62@gmail.com>
2023-02-28 17:49:32 -0700

    Write comments for clarity

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-
+// Targeting html elements for searching button and for city being searched
 let searchIN = document.getElementById("lookUpCity")
 let searchBTN = document.getElementById("btnS")
 
+// Array for previous cities
 let listCities = []
 
 
 
+// Event listener on click search button sets up local storage for previous cities
 searchBTN.addEventListener("click",search)
 function search() {
     let city = searchIN.value
@@ -15,13 +17,14 @@ function search() {
     console.log(city)
 
 }
-
+// get API setting for city and using imperial format for numbers
 function getApi(city) {
     let urlRequest ="https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=dc343b9765250b6d21dfd17c4cba48a5&units=imperial";
     fetch(urlRequest)
     .then(function (response) {
       return response.json();
     })
+    // Setting up jquery to input temp,wind speed, and humidity
     .then(function (data) {
         $("#yourCity").html(city)
         $("#presentDay").text(dayjs().format('(MMM/D/YYYY)'))
@@ -31,6 +34,7 @@ function getApi(city) {
     })
 }
 
+// function to render te previous cities that the user searches for
 function renderPreviousCities() {
     let previousCities = JSON.parse(localStorage.getItem('Previous Searches'))
     if (previousCities !== null) {
@@ -39,7 +43,7 @@ function renderPreviousCities() {
         return
     }
 }
-
+// On click display previous cities
 searchBTN.addEventListener("click", renderPreviousCities)
 
 
